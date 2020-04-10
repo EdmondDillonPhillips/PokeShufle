5e6fb8a7107782059999aa5473dbe96c1705773c
EdmondDillonPhillips <edmond.dillon.phillips@gmail.com>
2020-04-10 00:53:24 -0400

    modified server.js file

--- Avatar-Shuffle/server.js
+++ Avatar-Shuffle/server.js
@@ -1,13 +1,30 @@
+// const express = require('express');
+
+// const app = express();
+
+// app.use(express.static('./dist/avatar-shuffle'));
+
+// app.get('/*', function(req, res) {
+//     res.sendFile('index.html', {root: 'dist/avatar-shuffle/'});
+//   });
+
+//   app.listen(process.env.PORT || 8080, ()=>{
+//       console.log("Server has started");
+//   });
+
+  //Install express server
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
-app.use(express.static('./dist/avatar-shuffle'));
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/avatar-shuffle'));
 
-app.get('/*', function(req, res) {
-    res.sendFile('index.html', {root: 'dist/avatar-shuffle/'});
-  });
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/avatar-shuffle/index.html'));
+});
 
-  app.listen(process.env.PORT || 8080, ()=>{
-      console.log("Server has started");
-  });
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
