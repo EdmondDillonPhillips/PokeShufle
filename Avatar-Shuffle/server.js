c6614f836baf84ba055707359c0a68f8ab313e83
EdmondDillonPhillips <edmond.dillon.phillips@gmail.com>
2020-04-10 00:57:18 -0400

    modified user method in server.js

--- Avatar-Shuffle/server.js
+++ Avatar-Shuffle/server.js
@@ -19,12 +19,18 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/avatar-shuffle'));
+// app.use(express.static(__dirname + '/dist/avatar-shuffle'));
+app.use(express.static('AvatarShuffle/dist/avatar-shuffle'));
+
 
 app.get('/*', function(req,res) {
     
 res.sendFile(path.join(__dirname+'/dist/avatar-shuffle/index.html'));
 });
 
+console.log(__dirname);
+
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8080, ()=>{
+    console.log("server has started on port 8080")
+});
